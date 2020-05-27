45b7da688fe7f26db0298e01f97d68b208830b75
cycychenyi <chenyi111@whu.edu.cn>
2020-05-27 16:50:51 +0800

    feature: 2020 spring

---
+++ 2020_spring/pingjiao.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import platform
+import time
+
+import click as click
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+
+def init_driver() -> Chrome:
+    # 初始化 driver
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    if 'Windows' in platform.platform():
+        executable_path = os.path.join(os.path.dirname(__file__), 'chromedriver.exe')
+    else:
+        executable_path = os.path.join(os.path.dirname(__file__), 'chromedriver')
+    driver = Chrome(executable_path=executable_path, options=chrome_options)
+    driver.implicitly_wait(3)
+    return driver
+
+
+def login(driver: Chrome, username: str, password: str) -> None:
+    driver.get('http://s.ugsq.whu.edu.cn/caslogin')
+    username_input = driver.find_element_by_xpath('//*[@id="username"]')
+    username_input.send_keys(username)
+    password_input = driver.find_element_by_xpath('//*[@id="password"]')
+    password_input.send_keys(password)
+    login_button = driver.find_element_by_xpath('//*[@id="casLoginForm"]/p[5]/button')
+    login_button.click()
+
+
+def pingjia(driver: Chrome) -> None:
+    # 限制不能给满分，第一个选项四星
+    driver.find_element_by_xpath('//div[@class="controls" and label[@class="radio"]]/label[2]').click()
+    # 其他选项全部五星
+    labels = driver.find_elements_by_xpath('//div[@class="controls" and label[@class="radio"]]/label[1]')[1:]
+    for label in labels:
+        label.click()
+    # 意见填无
+    textarea = driver.find_element_by_xpath('//*[@id="pjnr"]/li[7]/fieldset/ol/li/div[3]/div/textarea')
+    textarea.send_keys('无')
+    submit_button = driver.find_element_by_xpath('//*[@id="pjsubmit"]')
+    submit_button.click()
+    # 评教成功后关闭弹窗
+    close_button = driver.find_element_by_xpath('//*[@id="finishDlg"]/div[2]/button')
+    close_button.click()
+
+
+def pingjia_per_page(driver: Chrome, all_pingjiaed: bool, count: int) -> int:
+    kcs = driver.find_elements_by_xpath('//*[@id="pjkc"]/tr')
+    for kc in kcs:
+        if not all_pingjiaed:
+            break
+        if kc.find_element_by_xpath('td[5]').text == '已评价':
+            continue
+        kc.find_element_by_xpath('td[6]/a').click()
+        pingjia(driver)
+        all_pingjiaed = False
+        count += 1
+        time.sleep(3)
+    return count
+
+
+@click.command()
+@click.option('--username', prompt='学号')
+@click.option('--password', prompt='信息门户密码（默认身份证后 6 位）')
+def pingjiao(username: str, password: str) -> int:
+    count = 0
+    driver = init_driver()
+    login(driver, username, password)
+    all_pingjiaed = False
+
+    while not all_pingjiaed:
+        all_pingjiaed = True
+        driver.get('http://s.ugsq.whu.edu.cn/studentpj')
+        driver.find_element_by_xpath('//*[@id="task-list"]/li').click()
+        # 首页
+        count = pingjia_per_page(driver, all_pingjiaed, count)
+        if not all_pingjiaed:
+            continue
+        # 剩余页
+        pages = driver.find_elements_by_xpath('//*[@id="tb1_wrapper"]/div/ul/li/a')[2:-1]
+        for page in pages:
+            if not all_pingjiaed:
+                break
+            page.click()
+            time.sleep(3)
+            count = pingjia_per_page(driver, all_pingjiaed, count)
+
+    print(f'共评价了 {count} 门课程')
+    return count
+
+
+if __name__ == '__main__':
+    pingjiao()
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-## 武汉大学评教脚本
+## 武汉大学评教脚本 v1.0
 
 用 Python 写的脚本，用于 2019 年秋季评教。
 
@@ -6,7 +6,7 @@
 
 注意，由于我的课程只有 10 个，刚好在一页里显示了，多页的情况可能有所不同。
 
-## 食用方法
+**使用**
 
 1. 安装 Python。
 2. 安装第三方库。
@@ -25,9 +25,33 @@ git clone https://github.com/cycychenyi/PingJiao.git
 
 4. 在 pingjiao.py 所在目录按住 Shift 右键打开 Powershell 窗口。
 
-![](https://i.loli.net/2019/12/15/vVTMrnqWZilhgO1.png)
+<div align="center"><img src="https://i.loli.net/2019/12/15/vVTMrnqWZilhgO1.png" style="zoom: 67%;" /></div>
 
 5. 运行。
 
-![](https://i.loli.net/2019/12/15/rj3ApLq6Zfwig9D.png)
+<div align="center"><img src="https://i.loli.net/2019/12/15/rj3ApLq6Zfwig9D.png" style="zoom:80%;" /></div>
 
+## 武汉大学评教脚本 v2.0
+
+改用 Selenium，用于 2020 年春季评教。
+
+**使用**
+
+1. 安装 Python。
+2. 安装第三方库。
+
+```bash
+pip install click selenium
+# 或者
+pip install -r requirements.txt
+```
+
+3. 克隆仓库。
+
+```bash
+git clone https://github.com/cycychenyi/PingJiao.git
+```
+
+chromedriver 目录下的 chromedriver.exe 和 chromedriver 对应 83 版本的 Chrome，其它版本请自行下载。
+
+4. 运行 pingjiao.py。
