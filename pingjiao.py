17b45e3608b4ae8c76e9b6f68182ccb821fae3fb
Crescentm <guyues@outlook.com>
2022-01-04 22:32:32 +0800

    增加自动下载chromedriver的功能

--- pingjiao.py
+++ pingjiao.py
@@ -3,11 +3,89 @@
 import os
 import platform
 import time
-
+import requests,winreg,zipfile,re
 import click as click
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
+
+
+url='https://chromedriver.storage.googleapis.com/' # chromedriver download link
+
+def get_Chrome_version():
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Google\Chrome\BLBeacon')
+    version, types = winreg.QueryValueEx(key, 'version')
+    return version
+
+def get_latest_version(url):
+    '''查询最新的Chromedriver版本'''
+    rep = requests.get(url).text
+    time_list = []                                          # 用来存放版本时间
+    time_version_dict = {}                                  # 用来存放版本与时间对应关系
+    result = re.compile(r'\d.*?/</a>.*?Z').findall(rep)     # 匹配文件夹（版本号）和时间
+    for i in result:
+        time = i[-24:-1]                                    # 提取时间
+        version = re.compile(r'.*?/').findall(i)[0]         # 提取版本号
+        time_version_dict[time] = version                   # 构建时间和版本号的对应关系，形成字典
+        time_list.append(time)                              # 形成时间列表
+    latest_version = time_version_dict[max(time_list)][:-1] # 用最大（新）时间去字典中获取最新的版本号
+    return latest_version
+
+def get_server_chrome_versions():
+    '''return all versions list'''
+    versionList=[]
+    url="http://npm.taobao.org/mirrors/chromedriver/"
+    rep = requests.get(url).text
+    result = re.compile(r'\d.*?/</a>.*?Z').findall(rep)
+    for i in result:                                 # 提取时间
+        version = re.compile(r'.*?/').findall(i)[0]         # 提取版本号
+        versionList.append(version[:-1])                  # 将所有版本存入列表
+    return versionList
+
+
+def download_driver(download_url):
+    '''下载文件'''
+    file = requests.get(download_url)
+    with open("chromedriver.zip", 'wb') as zip_file:        # 保存文件到脚本所在目录
+        zip_file.write(file.content)
+
+def get_version():
+    '''查询系统内的Chromedriver版本'''
+    outstd2 = os.popen('.\chromedriver.exe --version').read()
+    if len(outstd2) <=0:
+        return "0.0"
+    return outstd2.split(' ')[1]
+
+
+def unzip_driver():
+    '''解压Chromedriver压缩包到指定目录'''
+    f = zipfile.ZipFile("chromedriver.zip",'r')
+    for file in f.namelist():
+        f.extract(file)
+
+def check_update_chromedriver():
+    chromeVersion=get_Chrome_version()
+    chrome_main_version=int(chromeVersion.split(".")[0]) # chrome主版本号
+    driverVersion=get_version()
+    driver_main_version=int(driverVersion.split(".")[0]) # chromedriver主版本号
+    download_url=""
+    if driver_main_version!=chrome_main_version:
+        print("Downloading/Updating chromedriver")
+        versionList=get_server_chrome_versions()
+        if chromeVersion in versionList:
+            download_url=f"{url}{chromeVersion}/chromedriver_win32.zip"
+        else:
+            for version in versionList:
+                if version.startswith(str(chrome_main_version)):
+                    download_url=f"{url}{version}/chromedriver_win32.zip"
+                    break
+            if download_url=="":
+                print("Cannot find chromedriver")
+
+        download_driver(download_url=download_url)
+        unzip_driver()
+        os.remove("chromedriver.zip")
+        print('Chromedriver version:', get_version())
 
 def init_driver() -> Chrome:
     # 初始化 driver
@@ -113,4 +191,9 @@ def pingjiao(username: str, password: str):
 
 
 if __name__ == '__main__':
+    if 'Windows' in platform.platform():
+        try:
+            check_update_chromedriver()
+        except:
+            print("Cannot Download chromedriver")
     pingjiao()
--- readme.md
+++ readme.md
@@ -5,56 +5,22 @@
 **使用**
 
 1. 安装 Python。
-2. 安装第三方库。
+2. 克隆仓库。
 
 ```bash
-pip install click selenium
-# 或者
+git clone https://github.com/cycychenyi/PingJiao.git
+```
+
+3. 下载chromedriver，windows下会自动尝试安装，Linux/macos请自行下载
+4. 安装第三方库。
+
+```bash
 pip install -r requirements.txt
 ```
 
-3. 克隆仓库。
+5. 运行 pingjiao.py。
 
 ```bash
-git clone https://github.com/cycychenyi/PingJiao.git
+python pingjiao.py
 ```
-
-chromedriver 目录下的 chromedriver.exe 和 chromedriver 对应 91 版本的 Chrome，其它版本请自行下载。
-
-下载链接: https://chromedriver.storage.googleapis.com/index.html
-
-4. 运行 pingjiao.py。
-
-## 武汉大学评教脚本 v1.0
-
-用 Python 写的脚本，用于 2019 年秋季评教。
-
-码字过程记录在 [知乎](https://zhuanlan.zhihu.com/p/97320141) 了。
-
-注意，由于我的课程只有 10 个，刚好在一页里显示了，多页的情况可能有所不同。
-
-**使用**
-
-1. 安装 Python。
-2. 安装第三方库。
-
-```bash
-pip install requests lxml PyExecJS click
-```
-
-3. 克隆仓库。
-
-```bash
-git clone https://github.com/cycychenyi/PingJiao.git
-```
-
-或直接下载 pingjiao.py 和 encrypt.js，注意放在同一目录下。
-
-4. 在 pingjiao.py 所在目录按住 Shift 右键打开 Powershell 窗口。
-
-<div align="center"><img src="https://i.loli.net/2019/12/15/vVTMrnqWZilhgO1.png" style="zoom: 67%;" /></div>
-
-5. 运行。
-
-<div align="center"><img src="https://i.loli.net/2019/12/15/rj3ApLq6Zfwig9D.png" style="zoom:80%;" /></div>
 
